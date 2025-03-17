44f968ba85e2a85db234c1a03834cbe5876518af
Clarets614 <cstock614@gmail.com>
2025-03-16 23:41:25 -0400

    fixed

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
@@ -17,7 +18,7 @@ def main():
     
     player = Player(x, y)
     
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
     
 
     
@@ -25,12 +26,14 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        #player.update(dt)
+        
         updatable.update(dt)
         
         screen.fill((0,0,0))
         
-        drawable.draw(screen)
+        
+        for obj in drawable:
+            obj.draw(screen)
 
         
         pygame.display.flip()
