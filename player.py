10e0fd4f5a1d481160d5dacd1775f21643348c15
Clarets614 <cstock614@gmail.com>
2025-03-18 16:10:32 -0400

    cooldown working

--- player.py
+++ player.py
@@ -7,7 +7,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x,y,PLAYER_RADIUS)
         self.rotation = 0
-        timer = 0
+        self.timer = 0
 
         
 
@@ -28,6 +28,7 @@ class Player(CircleShape):
         
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.timer -= dt
 
         if keys[pygame.K_a]:
             self.rotate(-dt)
@@ -38,7 +39,10 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(-dt)
         if keys[pygame.K_SPACE]:
-            self.shoot()           
+            if self.timer <= 0:
+                self.shoot()
+                self.timer = PLAYER_SHOOT_COOLDOWN 
+                      
         
             
     def move(self, dt):
